d6a2ed14c46c766db847178b68d69b5cc81d608e
Lucca <lucca.rocha@outlook.com>
2025-02-12 16:02:58 -0300

    add folder ExercicioBeecrowd1004

---
+++ ExercicioBeecrowd1004/ExercicioBeecrowd1004/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, PROD;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            PROD = a * b;
+
+            Console.WriteLine("PROD = " + PROD);
+
+        }
+    }
+}
