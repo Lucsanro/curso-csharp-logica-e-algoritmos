18101b76ac0ee301d4a6e53accd374664ebb3ce4
Lucca <lucca.rocha@outlook.com>
2025-02-12 15:47:47 -0300

    add folder ExercicioBeecrowd1002

---
+++ ExercicioBeecrowd1002/ExercicioBeecrowd1002/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159;
+            double area;
+            double raio;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * Math.Pow(raio, 2);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
