00b203408f366b001001e878cf4095662e5b5c82
Lucca <lucca.rocha@outlook.com>
2025-02-13 11:50:10 -0300

    add folder ExercicioBeecrowd1016

---
+++ ExercicioBeecrowd1016/ExercicioBeecrowd1016/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carro1 = 60, carro2 = 90;
+
+            int km = int.Parse(Console.ReadLine());
+
+            int distanciaEntreCarrosEmMinutos = km * 2;
+
+            Console.WriteLine(distanciaEntreCarrosEmMinutos + " minutos");
+        }
+    }
+}
