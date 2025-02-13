dd960fdb6fb8bcc4a73265f5402e60e70476f037
Lucca <lucca.rocha@outlook.com>
2025-02-13 13:38:24 -0300

    add folder ExercicioBeecrowd1011

---
+++ ExercicioBeecrowd1011/ExercicioBeecrowd1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, pi = 3.14159, volume;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            volume = (double)(4.0 / 3.0) * pi * Math.Pow(raio, 3.0);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
