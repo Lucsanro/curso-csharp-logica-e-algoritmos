7ba789c399b7573807b481d0e7fbb346b96584e1
Lucca <lucca.rocha@outlook.com>
2025-02-13 13:27:44 -0300

    add folder ExercicioBeecrowd1009

---
+++ ExercicioBeecrowd1009/ExercicioBeecrowd1009/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, vendas;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioTotal = salario + vendas * 0.15;
+
+            Console.WriteLine("TOTAL = R$ " + salarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
