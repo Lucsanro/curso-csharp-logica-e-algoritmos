1bbbbea20b673d1a8c75c63ce346240e8c6a2737
Lucca <lucca.rocha@outlook.com>
2025-02-13 11:27:45 -0300

    add folder ExercicioBeecrowd1014

---
+++ ExercicioBeecrowd1014/ExercicioBeecrowd1014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanciaTotal = int.Parse(Console.ReadLine());
+            double combustivelTotal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double consumoMedio = distanciaTotal / combustivelTotal;
+
+            Console.WriteLine(consumoMedio.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
