9d85b816ccb68c506a61c7c1dffad8d6c04542d8
Lucca <lucca.rocha@outlook.com>
2025-02-13 12:11:06 -0300

    add folder ExercicioBeecrowd1017

---
+++ ExercicioBeecrowd1017/ExercicioBeecrowd1017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo, velocidade, consumoAutomovel = 12;
+
+            tempo = int.Parse(Console.ReadLine());
+            velocidade = int.Parse(Console.ReadLine());
+
+            double combustivel = (double) tempo * velocidade / consumoAutomovel;
+
+            Console.WriteLine(combustivel.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
