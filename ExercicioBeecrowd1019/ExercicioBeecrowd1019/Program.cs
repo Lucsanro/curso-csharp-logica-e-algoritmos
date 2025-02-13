4247230b354b8c4d4679dbca1f18ee6ddeb822ea
Lucca <lucca.rocha@outlook.com>
2025-02-13 17:42:35 -0300

    add folder ExercicioBeecrowd1019

---
+++ ExercicioBeecrowd1019/ExercicioBeecrowd1019/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int horaEmSegundos = 3600, minutoEmSegundos = 60;
+
+            int hora = n / horaEmSegundos;
+            int minutos = (n % horaEmSegundos) / minutoEmSegundos;
+            int segundos = (n % horaEmSegundos) % minutoEmSegundos;
+
+            Console.WriteLine(hora + ":" + minutos + ":" + segundos);
+        }
+    }
+}
