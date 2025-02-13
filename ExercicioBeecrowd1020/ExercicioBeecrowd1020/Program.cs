10e7b230ac2e4e6d54c798f5106991a33c5d6b5d
Lucca <lucca.rocha@outlook.com>
2025-02-13 19:35:30 -0300

    add folder ExercicioBeecrowd1020

---
+++ ExercicioBeecrowd1020/ExercicioBeecrowd1020/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeEmDias = int.Parse(Console.ReadLine());
+
+            int resto = idadeEmDias;
+
+            int ano = 365;
+            int calculo = resto / ano;
+            Console.WriteLine(calculo + " ano(s)");
+            resto = resto % ano;
+
+            int mes = 30;
+            calculo = resto / mes;
+            Console.WriteLine(calculo + " mes(es)");
+            resto = resto % mes;
+
+            int dia = 1;
+            calculo = resto / dia;
+            Console.WriteLine(calculo + " dia(s)");
+
+        }
+    }
+}
