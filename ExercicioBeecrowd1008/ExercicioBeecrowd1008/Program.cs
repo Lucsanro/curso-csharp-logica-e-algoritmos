0eac033e7b1c99b5062a9469ff9fa9f732534077
Lucca <lucca.rocha@outlook.com>
2025-02-13 09:49:49 -0300

    add folder ExercicioBeecrowd1008

---
+++ ExercicioBeecrowd1008/ExercicioBeecrowd1008/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFuncionario, horasTrabalhadas;
+            double valorDaHora, salario;
+
+            numeroFuncionario = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            valorDaHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = valorDaHora * horasTrabalhadas;
+
+            Console.WriteLine("NUMBER = " + numeroFuncionario);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
