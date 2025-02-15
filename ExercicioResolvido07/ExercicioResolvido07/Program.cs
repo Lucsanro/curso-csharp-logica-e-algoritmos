8fa0ec93efe7a1b144d1b0d99c54559d0cd7f710
Lucca <lucca.rocha@outlook.com>
2025-02-15 17:36:39 -0300

    add folder ExercicioResolvido07

---
+++ ExercicioResolvido07/ExercicioResolvido07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioResolvido07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+            a = int.Parse(vetor[0]);
+            b = int.Parse(vetor[1]);
+            c = int.Parse(vetor[2]);
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine("MENOR = " + a);
+            }
+            else if (b < c)
+            {
+                Console.WriteLine("MENOR = " + b);
+            }
+            else
+            {
+                Console.WriteLine("MENOR = " + c);
+
+            }
+        }
+    }
+}
