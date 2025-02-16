476c4da39bf52ea795bea42902d4c9504ffca90d
Lucca <lucca.rocha@outlook.com>
2025-02-16 14:05:39 -0300

    add folder ExercicioBeecrowd1046

---
+++ ExercicioBeecrowd1046/ExercicioBeecrowd1046/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Beecrowd1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal, tempoDeJogo;
+
+            string[] vetor = Console.ReadLine().Split();
+            horaInicial = int.Parse(vetor[0]);
+            horaFinal = int.Parse(vetor[1]);
+
+            if (horaInicial > horaFinal)
+            {
+                tempoDeJogo = (24 - horaInicial) + horaFinal;
+                Console.WriteLine("O JOGO DUROU " + tempoDeJogo + " HORA(S)");
+            }
+            else if (horaFinal > horaInicial)
+            {
+                tempoDeJogo = horaFinal - horaInicial;
+                Console.WriteLine("O JOGO DUROU " + tempoDeJogo + " HORA(S)");
+            }
+            else
+            {
+                tempoDeJogo = 24;
+                Console.WriteLine("O JOGO DUROU " + tempoDeJogo + " HORA(S)");
+            }
+
+
+        }
+    }
+}
