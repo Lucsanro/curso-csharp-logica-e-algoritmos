7d5c2667c8fe9304ff998f649ce9b6441a57d2fd
Lucca <lucca.rocha@outlook.com>
2025-02-17 12:16:18 -0300

    add folder ExercicioBeecrowd1037

---
+++ ExercicioBeecrowd1037/ExercicioBeecrowd1037/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (numero >= 0.0 && numero <= 25.0)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (numero > 25.0 && numero <= 50.0)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (numero > 50.0 && numero <= 75.0)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else if (numero > 75.0 && numero <= 100.0)
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+            else
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+        }
+    }
+}
