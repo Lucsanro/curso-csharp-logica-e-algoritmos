fec421e5046f5eee0810eb44e281f804638e41d5
Lucca <lucca.rocha@outlook.com>
2025-02-17 20:24:09 -0300

    add folder ExercicioBeecrowd1049

---
+++ ExercicioBeecrowd1049/ExercicioBeecrowd1049/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string esquema1, esquema2, esquema3, animal;
+
+            esquema1 = Console.ReadLine();
+            esquema2 = Console.ReadLine();
+            esquema3 = Console.ReadLine();
+
+            if (esquema1 == "vertebrado")
+            {
+                if (esquema2 == "ave")
+                {
+                    if (esquema3 == "carnivoro")
+                    {
+                        animal = "aguia";
+                    }
+                    else
+                    {
+                        animal = "pomba";
+                    }
+                }
+                else
+                {
+                    if (esquema3 == "onivoro")
+                    {
+                        animal = "homem";
+                    }
+                    else
+                    {
+                        animal = "vaca";
+                    }
+                }
+            }
+            else
+            {
+                if (esquema2 == "inseto")
+                {
+                    if (esquema3 == "hematofago")
+                    {
+                        animal = "pulga";
+                    }
+                    else
+                    {
+                        animal = "lagarta";
+                    }
+                }
+                else
+                {
+                    if (esquema3 == "hematofago")
+                    {
+                        animal = "sanguessuga";
+                    }
+                    else
+                    {
+                        animal = "minhoca";
+                    }
+                }
+            }
+
+            Console.WriteLine(animal);
+        }
+    }
+}
