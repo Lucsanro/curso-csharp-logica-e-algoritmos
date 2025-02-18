c22e479d48c813e64e0164b0365941ec0095844a
Lucca <lucca.rocha@outlook.com>
2025-02-18 12:09:40 -0300

    add folder ExercicioBeecrowd1070

---
+++ ExercicioBeecrowd1070/ExercicioBeecrowd1070/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+
+            X = int.Parse(Console.ReadLine());
+
+            if (X % 2 != 0)
+            {
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+            }
+            else
+            {
+                X += 1;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+                X += 2;
+                Console.WriteLine(X);
+            }
+        }
+    }
+}
