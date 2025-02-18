4b3f96bf89835b68984e8fc81dd8bf85213ff21c
Lucca <lucca.rocha@outlook.com>
2025-02-18 10:51:21 -0300

    add folder ExercicioBeecrowd1052

---
+++ ExercicioBeecrowd1052/ExercicioBeecrowd1052/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDoMes = int.Parse(Console.ReadLine());
+            string month;
+
+            switch (numeroDoMes)
+            {
+                case 1:
+                    month = "January";
+                    Console.WriteLine(month);
+                    break;
+                case 2:
+                    month = "February";
+                    Console.WriteLine(month);
+                    break;
+                case 3:
+                    month = "March";
+                    Console.WriteLine(month);
+                    break;
+                case 4:
+                    month = "April";
+                    Console.WriteLine(month);
+                    break;
+                case 5:
+                    month = "May";
+                    Console.WriteLine(month);
+                    break;
+                case 6:
+                    month = "June";
+                    Console.WriteLine(month);
+                    break;
+                case 7:
+                    month = "July";
+                    Console.WriteLine(month);
+                    break;
+                case 8:
+                    month = "August";
+                    Console.WriteLine(month);
+                    break;
+                case 9:
+                    month = "September";
+                    Console.WriteLine(month);
+                    break;
+                case 10:
+                    month = "October";
+                    Console.WriteLine(month);
+                    break;
+                case 11:
+                    month = "November";
+                    Console.WriteLine(month);
+                    break;
+                case 12:
+                    month = "December";
+                    Console.WriteLine(month);
+                    break;
+            }
+        }
+    }
+}
