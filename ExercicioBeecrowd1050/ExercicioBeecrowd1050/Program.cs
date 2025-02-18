fc6aee2ca4ec5b91c0d1dd135cf066f87de592e8
Lucca <lucca.rocha@outlook.com>
2025-02-18 07:20:05 -0300

    add folder ExercicioBeecrowd1050

---
+++ ExercicioBeecrowd1050/ExercicioBeecrowd1050/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DDD = int.Parse(Console.ReadLine());
+            string destino;
+
+            switch (DDD)
+            {
+                case 61:
+                    destino = "Brasilia";
+                    break;
+                case 71:
+                    destino = "Salvador";
+                    break;
+                case 11:
+                    destino = "Sao Paulo";
+                    break;
+                case 21:
+                    destino = "Rio de Janeiro";
+                    break;
+                case 32:
+                    destino = "Juiz de Fora";
+                    break;
+                case 19:
+                    destino = "Campinas";
+                    break;
+                case 27:
+                    destino = "Vitoria";
+                    break;
+                case 31:
+                    destino = "Belo Horizonte";
+                    break;
+                default:
+                    destino = "DDD nao cadastrado";
+                    break;
+            }
+
+            Console.WriteLine(destino);
+        }
+    }
+}
