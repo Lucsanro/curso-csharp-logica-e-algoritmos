35661f1213dfe778e12a154cc337971078aa4856
Lucca <lucca.rocha@outlook.com>
2025-02-18 11:40:33 -0300

    add folder ExercicioBeecrowd1065

---
+++ ExercicioBeecrowd1065/ExercicioBeecrowd1065/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, contador = 0;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+
+            if (Math.Abs(a) % 2 == 0)
+            {
+                contador += 1;
+            }
+            if (Math.Abs(b) % 2 == 0)
+            {
+                contador += 1;
+            }
+            if (Math.Abs(c) % 2 == 0)
+            {
+                contador += 1;
+            }
+            if (Math.Abs(d) % 2 == 0)
+            {
+                contador += 1;
+            }
+            if (Math.Abs(e) % 2 == 0)
+            {
+                contador += 1;
+            }
+
+            Console.WriteLine(contador + " valores pares");
+        }
+    }
+}
