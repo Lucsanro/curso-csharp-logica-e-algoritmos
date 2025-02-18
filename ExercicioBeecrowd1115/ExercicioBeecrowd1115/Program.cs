40c28043e7fcebe787df64b6d9a49785741f39b9
Lucca <lucca.rocha@outlook.com>
2025-02-18 16:08:51 -0300

    add folder ExercicioBeecrowd1115

---
+++ ExercicioBeecrowd1115/ExercicioBeecrowd1115/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            string[] vetor = Console.ReadLine().Split();
+            x = int.Parse(vetor[0]);
+            y = int.Parse(vetor[1]);
+
+            while (x != 0 || y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                    vetor = Console.ReadLine().Split();
+                    x = int.Parse(vetor[0]);
+                    y = int.Parse(vetor[1]);
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                    vetor = Console.ReadLine().Split();
+                    x = int.Parse(vetor[0]);
+                    y = int.Parse(vetor[1]);
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                    vetor = Console.ReadLine().Split();
+                    x = int.Parse(vetor[0]);
+                    y = int.Parse(vetor[1]);
+                }
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("quarto");
+                    vetor = Console.ReadLine().Split();
+                    x = int.Parse(vetor[0]);
+                    y = int.Parse(vetor[1]);
+                }
+            }
+        }
+    }
+}
