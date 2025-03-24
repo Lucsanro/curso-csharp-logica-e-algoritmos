014a15182e13bc98a4853e08c2b0fbaec602eab5
Lucca <lucca.rocha@outlook.com>
2025-03-24 15:27:41 -0300

    add folder ExercicioBeecrowd1134

---
+++ ExercicioBeecrowd1134/ExercicioBeecrowd1134/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExercicioBeecrowd1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool, gasolina, diesel, combustivel;
+
+            alcool = 0;
+            gasolina = 0;
+            diesel = 0;
+
+            combustivel = int.Parse(Console.ReadLine());
+
+            while (combustivel != 4)
+            {
+                if (combustivel == 1)
+                {
+                    alcool += 1;
+                }
+                if (combustivel == 2)
+                {
+                    gasolina += 1;
+                }
+                if (combustivel == 3)
+                {
+                    diesel += 1;
+                }
+                combustivel = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
