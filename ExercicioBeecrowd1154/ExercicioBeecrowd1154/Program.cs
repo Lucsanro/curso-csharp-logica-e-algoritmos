07089b531607318b0124d1bbb71f19e052255bc3
Lucca <lucca.rocha@outlook.com>
2025-03-25 06:44:12 -0300

    add folder ExercicioBeecrowd1154

---
+++ ExercicioBeecrowd1154/ExercicioBeecrowd1154/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBeecrowd1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, somaIdade, contadorIdade;
+            double mediaIdade;
+
+            somaIdade = 0;
+            contadorIdade = 0;
+            idade = int.Parse(Console.ReadLine());
+
+            while (idade > 0)
+            {
+                somaIdade += idade;
+                contadorIdade += 1;
+                idade = int.Parse(Console.ReadLine());
+
+            }
+
+            mediaIdade = (double) somaIdade / contadorIdade;
+
+            Console.WriteLine(mediaIdade.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
